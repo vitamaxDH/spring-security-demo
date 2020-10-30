410205dc4eca22e19b8c10accaee0f1bd16fde6e
vitamaxDH <hellonewtry@gmail.com>
2020-10-30 23:54:57 +0900

    feat: UserDetail & OAuthUser

--- src/main/java/com/cos/security1/auth/PrincipalDetails.java
+++ src/main/java/com/cos/security1/auth/PrincipalDetails.java
@@ -14,11 +14,18 @@ import java.util.Map;
 public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user;
+    private Map<String, Object> attributes;
 
+    // 일반 로그인
     public PrincipalDetails(User user) {
         this.user = user;
     }
 
+    // OAuth 로그인
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
 
     // 해당 User의 권한을 리턴하는 곳!!
     @Override
@@ -72,7 +79,7 @@ public class PrincipalDetails implements UserDetails, OAuth2User {
 
     @Override
     public Map<String, Object> getAttributes() {
-        return null;
+        return attributes;
     }
 
 }
